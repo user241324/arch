31697e750113ac2cbf35d58721443017e3958abf
user241324 <tom.rob.one@gmail.com>
2026-01-11 13:50:50 -0700

    Update archscript.sh

--- archscript.sh
+++ archscript.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
-# Add more flatpaks to be installed
-# Add polkit gui (lxsession contains lxpolkit) for x11 
-# Figure out flatpak theming
 
+### DESCRIPTION ###
+# Arch Linux package and configuration installation script.
+# Only use with fresh minimal installations, otherwise the git clones may fail.
+
+### TO DO ###
+# Add polkit gui (lxsession contains lxpolkit) for x11
+# Confirm hyprpolkit works as currently set
+# Add if statement for SSH to allow users to say no and initiate reboot
+# Try to adjust yay installation to not require intervention
+# Add theme config files for GTK3/4, QT5/6, and SDDM.
+# Figure out flatpak theming
+# Consider changing script to allow config files to replace existing directories and files.
+
+### VARIABLES ###
 pacinstall="sudo pacman -S --needed --noconfirm"
 
-# GITCONFIGS
+### UPDATE ###
+sudo pacman -Syu
+
+### GITCONFIGS ###
 $pacinstall git
-sudo git clone https://github.com/user241324/backgrounds /usr/share/backgrounds
-sudo rm -r /usr/share/backgrounds/.git
-sudo rm /usr/share/backgrounds/README.md
-git clone https://github.com/user241324/gitconfigs $HOME/.config
-sudo rm -r $HOME/.config/.git
-sudo rm $HOME/.config/README.md
+git clone https://github.com/user241324/gitconfigs/HOME/.config $HOME/.config
 chmod +x $HOME/.config/polybar/launch.sh
-sudo mkdir /etc/sddm.conf.d
-sudo mv $HOME/.config/sddm/sddm.conf.d/default.conf /etc/sddm.conf.d/default.conf
-sudo rm -r $HOME/.config/sddm
-sudo mv $HOME/.config/.xsessionrc $HOME/.xsessionrc
+git clone https://github.com/user241324/gitconfigs/HOME/scripts $HOME/scripts
+git clone https://github.com/user241324/gitconfigs/HOME/.xsessionrc $HOME/.xsessionrc
+sudo git clone https://github.com/user241324/gitconfigs/etc/X11 /etc/X11
+sudo git clone https://github.com/user241324/gitconfigs/etc/sddm.conf.d /etc/sddm.conf.d
+sudo git clone https://github.com/user241324/gitconfigs/usr/share/backgrounds /usr/share/backgrounds
+sudo git clone https://github.com/user241324/gitconfigs/usr/share/rofi /usr/share/rofi
 
-# SSH
-$pacinstall openssh
-ssh-keygen -f $HOME/.ssh/id_ed25519
-echo "Please enter <user>@<hostname> to share your public key with an SSH server:"
-read sshServer
-ssh-copy-id -i $HOME/.ssh/id_25519.pub $sshServer
-
-# YAY
+### YAY ###
 $pacinstall git base-devel
 git clone https://aur.archlinux.org/yay.git $HOME/.yay
 cd .yay || exit
@@ -35,46 +39,40 @@ yay -Y --gendb
 yay -Syu --devel
 yay -Y --devel --save
 
-# CLI TOOLS
-$pacinstall bash-completion brightnessctl btop fastfetch git gnu-free-fonts man-db nano openssh speedtest-cli tailscale
+### CLI TOOLS ###
+$pacinstall bash-completion brightnessctl btop fastfetch gnu-free-fonts man-db nano speedtest-cli tailscale
 yay -S paccache-hook --noconfirm
 echo -e "\n# Run at shell startup\nfastfetch" >> $HOME/.bashrc
 
-# SDDM
+### SDDM ###
 $pacinstall qt5-declarative gnu-free-fonts sddm xorg-server xorg-xrandr
 sudo systemctl enable sddm.service
 
-# HYPRLAND
+### HYPRLAND ###
 $pacinstall hyprland hyprpaper hyprpolkitagent otf-font-awesome waybar wofi
 
-# I3
-$pacinstall feh i3-wm polybar rofi # Add polkit agent for x11
+### I3 ###
+$pacinstall feh i3-wm polybar rofi
 
-# nwg-look
+### GTK THEMING ###
 $pacinstall breeze-gtk nwg-look
 
-# Qt theming
+### QT THEMING ###
 $pacinstall breeze qt5-wayland qt6-wayland
 yay -S qt5ct-kde qt6ct-kde --noconfirm
 
-########################
-### GUI APPLICATIONS ###
-########################
-
+### APPLICATIONS ###
 $pacinstall alacritty firefox libreoffice-fresh mousepad pavucontrol qbittorrent steam vlc
 yay -S betterbird-bin libation visual-studio-code.bin --noconfirm
 
-# virt-manager
+# VIRT-MANAGER
 $pacinstall dmidecode dns-masq libvirt qemu-full swtpm virt-manager
 sudo systemctl enable libvirtd.socket
 
-# thunar
-$pacinstall gvfs thunar thunar-archive-plugin thunar-volman xarchiver
+### THUNAR ###
+$pacinstall gvfs gvfs-smb thunar thunar-archive-plugin thunar-volman xarchiver
 
-###############
 ### FLATPAK ###
-###############
-
 $pacinstall flatpak
 
 avidemux=org.avidemux.Avidemux
@@ -93,10 +91,14 @@ protonupqt=net.davidotek.pupgui2
 ungoogledchromium=io.github.ungoogled_software.ungoogled_chromium
 vscode=com.visualstudio.code
 
-flatpak install -y $avidemux $bottles
+flatpak install -y $flatseal $bitwarden
 
-##############
+### SSH ###
+$pacinstall openssh
+ssh-keygen -f $HOME/.ssh/id_ed25519
+echo "Please enter <user>@<hostname> to share your public key with an SSH server:"
+read sshServer
+ssh-copy-id -i $HOME/.ssh/id_25519.pub $sshServer
+
 ### REBOOT ###
-##############
-
 sudo reboot now
