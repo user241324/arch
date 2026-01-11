0d309f3bfc8cc983fb78ef06e18d5fe17936a72c
user241324 <tom.rob.one@gmail.com>
2026-01-11 12:54:07 -0700

    Update and rename archbook.sh to chromebook.sh

--- chromebook.sh
+++ chromebook.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Script to install Arch on Chromebooks
-
 # Update installed packages
 sudo pacman -Syu --noconfirm
 
@@ -27,21 +25,3 @@ cd $HOME/.cros-keyboard-map || exit
 
 # Install mkinitcpio-numlock
 yay -S mkinitcpio-numlock --noconfirm
-
-# Install openssh
-sudo pacman -S --noconfirm openssh
-
-# Install hyprland
-sudo pacman -S --noconfirm hyprland hyprpaper otf-font-awesome waybar wofi
-
-sudo pacman -S alacritty brightnessctl fastfetch grim sof-firmware
-
-sudo pacman -S adw-gtk-theme nwg-look
-
-
-# File management with thunar
-sudo pacman -S --noconfirm file-roller gvfs thunar thunar-archive-plugin thunar-volman
-
-#Install other packages
-sudo pacman -S --noconfirm baobab chromium dnsmasq docker docker-compose fastfetch firefox keyd libreoffice-fresh qbittorrent tailscale wev
-yay -S corefm libcprime libcsys pulseaudio-nextsink python-setuptools-reproducible visual-studio-code-bin betterbird --noconfirm
